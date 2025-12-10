95bce5270d2793ebfb1d89055e23cb1b74c0ee37
JosHub911 <166252789+JosHub911@users.noreply.github.com>
2025-12-10 12:56:15 +0100

    Added lifes scripts (undone)

---
+++ Assets/Scripts/Player/PlayerLifes.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using TMPro;
+
+
+public class PlayerLifes : MonoBehaviour
+{
+    public int lifes = 3;
+    public TextMeshProUGUI lifesText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+}

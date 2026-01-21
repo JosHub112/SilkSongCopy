af93caa767f6f8b8ac248b99ff3ce53d77815a3a
JosHub911 <166252789+JosHub911@users.noreply.github.com>
2026-01-21 10:23:11 +0100

    UI works with animations

--- Assets/Scripts/Player/HealthUI.cs
+++ Assets/Scripts/Player/HealthUI.cs
@@ -1,24 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class HealthManager : MonoBehaviour
 {
     public static HealthManager Instance;
 
-    public List<Image> lifeImages = new List<Image>();
-
-    [Header("Sprites")]
-    public Sprite fullSprite;
-    public Sprite hitSprite;
-    public Sprite emptySprite;
-
-    [Header("Animation")]
-    public float hitDuration = 0.15f;
+    [Header("Life Animators")]
+    public List<Animator> lifeAnimators = new List<Animator>();
 
     private int lastLives = -1;
-    private Dictionary<int, Coroutine> anims = new Dictionary<int, Coroutine>();
 
     void Awake()
     {
@@ -29,82 +19,76 @@ public class HealthManager : MonoBehaviour
             return;
         }
 
-        if (lifeImages == null || lifeImages.Count == 0)
+        // Auto-fill from Health parent if empty
+        if (lifeAnimators.Count == 0)
         {
             var healthGO = GameObject.Find("Health");
             if (healthGO != null)
             {
                 foreach (Transform child in healthGO.transform)
                 {
-                    var img = child.GetComponent<Image>();
-                    if (img != null) lifeImages.Add(img);
+                    var anim = child.GetComponent<Animator>();
+                    if (anim != null)
+                        lifeAnimators.Add(anim);
                 }
 
-                lifeImages.Sort((a, b) =>
-                    a.transform.GetSiblingIndex().CompareTo(b.transform.GetSiblingIndex()));
+                lifeAnimators.Sort((a, b) =>
+                    a.transform.GetSiblingIndex()
+                    .CompareTo(b.transform.GetSiblingIndex()));
             }
         }
-
-        if (fullSprite == null && lifeImages.Count > 0)
-            fullSprite = lifeImages[0].sprite;
     }
 
     public void SetLives(int currentLives)
     {
-        int total = lifeImages.Count;
+        int total = lifeAnimators.Count;
         currentLives = Mathf.Clamp(currentLives, 0, total);
 
-        // First time setup
+        // First setup
         if (lastLives == -1)
         {
-            for (int i = 0; i < total; i++)
-                lifeImages[i].sprite = i < currentLives ? fullSprite : emptySprite;
-
             lastLives = currentLives;
             return;
         }
 
-        // Lost lives → animate
+        // Lost lives
         if (currentLives < lastLives)
         {
             for (int i = currentLives; i < lastLives; i++)
-                PlayHitAnimation(i);
+            {
+                PlayBreakAnimation(i);
+            }
         }
 
-        // Gained lives → instant refill
+        // Gained lives (reset to glimmer)
         if (currentLives > lastLives)
         {
             for (int i = lastLives; i < currentLives; i++)
             {
-                StopAnim(i);
-                lifeImages[i].sprite = fullSprite;
+                ResetLife(i);
             }
         }
 
         lastLives = currentLives;
     }
 
-    void PlayHitAnimation(int index)
+    void PlayBreakAnimation(int index)
     {
-        StopAnim(index);
-        anims[index] = StartCoroutine(HitRoutine(index));
+        var anim = lifeAnimators[index];
+
+        anim.SetTrigger("Break");
+        anim.SetTrigger("Hit");
+        
     }
 
-    IEnumerator HitRoutine(int index)
+    void ResetLife(int index)
     {
-        lifeImages[index].sprite = hitSprite;
-        yield return new WaitForSeconds(hitDuration);
-        lifeImages[index].sprite = emptySprite;
+        var anim = lifeAnimators[index];
+
+        anim.ResetTrigger("Break");
+        anim.ResetTrigger("Hit");
+        anim.Play("MaskGlimmer", 0, 0f);
     }
 
-    void StopAnim(int index)
-    {
-        if (anims.TryGetValue(index, out Coroutine c))
-        {
-            StopCoroutine(c);
-            anims.Remove(index);
-        }
-    }
-
-    public int MaxLives() => lifeImages.Count;
+    public int MaxLives() => lifeAnimators.Count;
 }

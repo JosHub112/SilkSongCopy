b9c38cbc6b32c5aa7465fe354ead35533c5a8d2c
JosHub911 <166252789+JosHub911@users.noreply.github.com>
2026-01-27 11:11:29 +0100

    Enemy

--- Assets/Scripts/Enemie/enemy.cs
+++ Assets/Scripts/Enemie/enemy.cs
@@ -1,28 +1,52 @@
 using UnityEngine;
+using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
     public int maxHealth = 100;
     int currentHealth;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Shake parameters
+    public float shakeDuration = 0.07f; // How long the shake lasts
+    public float shakeMagnitude = 0.1f; // How much it shakes
+
     void Start()
     {
         currentHealth = maxHealth;
     }
 
-
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
 
+        // Start shake
+        StopAllCoroutines(); // Stop previous shakes if overlapping
+        StartCoroutine(Shake());
+
         if (currentHealth <= 0)
         {
-             Die();
+            Die();
+        }
+    }
+
+    IEnumerator Shake()
+    {
+        float elapsed = 0f;
+
+        while (elapsed < shakeDuration)
+        {
+            // Random offset added to current position
+            Vector3 randomOffset = Random.insideUnitSphere * shakeMagnitude;
+            randomOffset.z = 0; // Keep shake in 2D plane
+            transform.position += randomOffset;
+
+            elapsed += Time.deltaTime;
+            yield return null;
         }
     }
 
     void Die()
     {
-       Destroy(gameObject);
+        Destroy(gameObject);
     }
 }
